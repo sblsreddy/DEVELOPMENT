7ed1b7db395482e5d8530a406f12472738745e6f
sblsreddy <r14141@gmail.com>
2024-07-14 18:45:56 -0500

    Initial commit of Pipes introduction and we used first one titlecase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'pipes';
+  name: string | undefined;
+
+  onNameChange(target: EventTarget) {
+      const value = (<HTMLInputElement>target).value;
+      console.log(value);
+      this.name = value;
+  }
 }
