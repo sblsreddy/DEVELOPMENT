aacfba8f2333318830446dff46f36bb46afca457
sblsreddy <r14141@gmail.com>
2024-07-14 19:20:12 -0500

    Updated with bootstrap style sheet and used date and currency pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   name: string | undefined;
+  date: string | undefined;
+  amount: string | undefined;
 
   onNameChange(target: EventTarget) {
-      const value = (<HTMLInputElement>target).value;
-      console.log(value);
-      this.name = value;
+    const value = (<HTMLInputElement>target).value;
+    console.log(value);
+    this.name = value;
+  }
+
+  onDateChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    console.log(value);
+    this.date = value;
+  }
+
+  onAmountChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    console.log(value);
+    this.amount = value;
   }
 }
