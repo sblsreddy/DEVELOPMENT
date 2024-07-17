f35039dab97ab68f8fdd4d010ad56bd995ebfeaf
sblsreddy <r14141@gmail.com>
2024-07-17 06:40:59 -0500

    Parent to child communication

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input} from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  constructor() { }
+  @Input() backgroundColor : string | undefined;
+  
+  constructor(private element : ElementRef) {     
+    console.log('Custom directive class used');
+    this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+  }
 
 }
+
+/**
+ *  Communcation from Parent to child component.....
+ * 
+ * In Parent component template ( in this scenario parent is app.component.html)
+ *  <app-card [title] = " 'snowyMountains' "> </app-card>
+ * 
+ * In Child component class ( in this scenario the child is class.directive.ts)
+ * we import like import {input} from '.angular'
+ * 
+ * class childComponent{
+ *    @Input() title:string;
+ * }
+ */
