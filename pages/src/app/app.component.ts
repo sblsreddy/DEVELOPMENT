3b304be2f50c3d894df07a6e385ec3a4817f592f
sblsreddy <r14141@gmail.com>
2024-07-17 05:45:29 -0500

    Introducing ng-container to reolve styling issues

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -27,6 +27,86 @@ export class AppComponent {
       title : 'Home',
       url : 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZXxlbnwwfHwwfHx8MA%3D%3D'
     },
+    {
+      title : 'Beach',
+      url : 'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJlYWNofGVufDB8fDB8fHww'
+    },
+    {
+      title : 'Forest',
+      url : 'https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'City',
+      url : 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Home',
+      url : 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Beach',
+      url : 'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJlYWNofGVufDB8fDB8fHww'
+    },
+    {
+      title : 'Forest',
+      url : 'https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'City',
+      url : 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Home',
+      url : 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Beach',
+      url : 'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJlYWNofGVufDB8fDB8fHww'
+    },
+    {
+      title : 'Forest',
+      url : 'https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'City',
+      url : 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Home',
+      url : 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Beach',
+      url : 'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJlYWNofGVufDB8fDB8fHww'
+    },
+    {
+      title : 'Forest',
+      url : 'https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'City',
+      url : 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Home',
+      url : 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Beach',
+      url : 'https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJlYWNofGVufDB8fDB8fHww'
+    },
+    {
+      title : 'Forest',
+      url : 'https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'City',
+      url : 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      title : 'Home',
+      url : 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZXxlbnwwfHwwfHx8MA%3D%3D'
+    }
   ];
 
 
